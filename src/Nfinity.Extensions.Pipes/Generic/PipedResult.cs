c8db8a1d387216f687b7f1f25421f407f09e3220
dvs <I24595@verisk.com>
2024-04-17 08:17:31 +0100

    poc

---
+++ src/Nfinity.Extensions.Pipes/Generic/PipedResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Nfinity.Extensions.Pipes.Generic
+{
+    public sealed class PipedResult
+    {
+        internal readonly static PipedResult Success = new(true);
+        internal readonly static PipedResult Fail = new(false);
+
+        public bool IsSuccess { get; }
+
+        private PipedResult(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+    }
+}
---
+++ src/Nfinity.Extensions.Pipes/Generic/PipedResultOfT.cs
@@ -0,0 +1,8 @@
+﻿namespace Nfinity.Extensions.Pipes.Generic
+{
+    public sealed class PipedResult<T>
+    {
+        public T Data { get; init; }
+        public bool IsSuccess { get; init; }
+    }
+}
